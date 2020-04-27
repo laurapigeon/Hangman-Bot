d3fed3ace174f1c00ed721d3c7c7b0134b8312af
Laura <laurahannah44@gmail.com>
2020-04-27 14:38:41 +0100

    Added comments and neatened

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 import copy
+import time
 import random
 
 
@@ -8,20 +9,20 @@ class Guesser:
     defaults = list("eaaaaeeeeeeiiiiiiiiioooee")  # first guess for different length words (hardcoded for efficency)
 
     @classmethod
-    def ask_length(cls):  # take input of and check validity of word length
+    def ask_length(cls):  # take input and check validity of word length
         while True:
             word_len = input("What is the length of your word? ")
             try:
                 return int(word_len)  # if integer (5)
             except:
-                if set(word_len) <= set("-"):  # if string of blanks (------)
+                if set(word_len) <= set(LETTER_CHARACTERS[0]):  # if string of blanks (------)
                     return len(word_len)
                 elif set(word_len) <= set(cls.letters):  # if word (triggers autoplay)
                     return word_len
                 print("That isn't a valid word length...")
 
     def __init__(self, length):  # creates a guesser-type with a given word length
-        self.letters = copy.copy(self.letters)
+        self.letters = copy.copy(self.letters)  # class attribute is mutable by default
 
         self.word_len = length
         self.create_word_list(self.word_len)
@@ -37,145 +38,165 @@ class Guesser:
             self.decide_letter()
 
     def create_word_list(self, length):  # gets the list of words with the given length (hardcoded for efficency)
-        words = open("english_words.txt", "r").read().split("\n \n")
+        words = open(WORDS_FILE, "r").read().split("\n \n")
         if words[length - 1]:
             self.words = words[length - 1].split("\n")
         else:
             self.words = list()
 
-    def ask_letter(self):
+    def ask_letter(self):  # take input and check validity of letter answer
         accurate = False
-        while not accurate:
+        while not accurate:  # repeat request until success
 
-            example = list()
+            example = list()  # create example string
             for _ in range(self.word_len):
-                example.append(("-", "=")[random.randint(0, 1)])
+                example.append(LETTER_CHARACTERS[random.randint(0, 1)])
             example = "".join(example)
 
             response = input("\nIs '{}' in the word? eg({}): ".format(self.guess, example))
 
-            if len(response) <= self.word_len:
-                response += "-" * (self.word_len - len(response))
-                if set(response) <= set(["-", "="]):
+            if len(response) <= self.word_len:  # fill remaining blanks after response
+                response += LETTER_CHARACTERS[0] * (self.word_len - len(response))
+                if set(response) <= set(LETTER_CHARACTERS):  # test response only contains true/false characters
                     accurate = True
-                    for index, character in enumerate(response):
-                        currently_accurate = (character == "=") and (self.cipher[index] == "-") or (character != "=")
-                        accurate = accurate and currently_accurate
-                    if not accurate:
+                    for index, character in enumerate(response):  # test for no overlaps with current string
+                        is_letter = character == LETTER_CHARACTERS[1]
+                        is_free = self.cipher[index] == "-"
+                        accurate = accurate and is_letter <= is_free  # a implies b
+                    if not accurate:  # if any errors are found, accurate becomes false
                         print("Character space already taken...")
                 else:
-                    print("Please only include =s and -s...")
+                    print("Please only include {}s and {}s...".format(*LETTER_CHARACTERS))
             else:
                 print("Please enter a valid length string...")
 
-        self.indices = list(response)
-        self.letters.remove(self.guess)
+        self.indices = response
+        self.letters.remove(self.guess)  # remove asked letter from set of letters
 
-    def build_letter(self):
+    def build_letter(self):  # for autoplay
         print("\nGuess: {}".format(self.guess))
         self.indices = str()
         for character in word:
-            self.indices += ("-", "=")[character == self.guess]
+            self.indices += LETTER_CHARACTERS[character == self.guess]
 
     def update_cipher(self):
         cipher = list(self.cipher)
         for index, truth in enumerate(self.indices):
-            if truth == "=":
+            if truth == LETTER_CHARACTERS[1]:
                 cipher[index] = self.guess
         self.cipher = "".join(cipher)
 
-    def get_words(self, letter, indices):
-        words = copy.copy(self.words)
-        removed_words = list()
-        for word in words:
-            if indices == "-" * self.word_len:
-                if letter in word:
-                    removed_words.append(word)
+    def get_words(self, letter, indices):  # gets new set of words given index restrictions
+        words = list()
+        for word in self.words:
+            if indices == LETTER_CHARACTERS[0] * self.word_len:
+                if letter not in word:  # efficient check for specific case
+                    words.append(word)
             else:
-                for index, truth in enumerate(indices):
-                    if (word[index] == letter) != (truth == "="):
-                        removed_words.append(word)
+                append = True
+                for index, truth in enumerate(indices):  # check for other cases
+                    if (word[index] == letter) != (truth == LETTER_CHARACTERS[1]):
+                        append = False
                         break
-
-        for word in removed_words:
-            words.remove(word)
+                if append:
+                    words.append(word)
 
         return words
 
     def check_words(self):
-        if len(self.words) == 1:
-            print("Your word is '{}'!".format(self.words[0]))
-            return True
-        elif len(self.words) == 0:
-            print("I don't know your word...")
-            return True
+        if len(self.words) in (0, 1):
+            if len(self.words) == 1:  # if word list reduced to single word
+                print("Your word is '{}'!".format(self.words[0]))
+            elif len(self.words) == 0:  # if word list reduced to empty
+                print("I don't know your word...")
+            return True  # found
+        else:
+            return False  # not found
 
-    def decide_letter(self):
+    def decide_letter(self):  # decide optimal letter to ask
         min_length = len(self.words)
         self.guess = self.letters[0]
+
         string = "Thinking"
-        for letter in self.letters:
+        for letter in self.letters:  # pick letter which reduces possible words the most if not in word
             print(string, end="\r")
-            current_length = len(self.get_words(letter, "-" * self.word_len))
-            if current_length < min_length and current_length != 0:
+            string += "."
+
+            start = time.time()
+
+            current_length = len(self.get_words(letter, LETTER_CHARACTERS[0] * self.word_len))
+
+            if current_length < min_length and current_length != 0:  # dont pick a letter which would remove all words
                 min_length = current_length
                 self.guess = letter
-            string += "."
+
+            end = time.time()
+            time.sleep(0.05 - end + start)
+
         print(" " * 100, end="\r")
 
     def update(self, word):
-        if word is None:
+        if word is None:  # gets self.indices
             self.ask_letter()
         else:
             self.build_letter()
 
         self.update_cipher()
-        if "=" not in self.indices:
+        if LETTER_CHARACTERS[1] not in self.indices:
             self.errors += 1
 
         print("\nCurrent word: {}\nErrors: {}".format(self.cipher, self.errors))
-        words = self.get_words(self.guess, self.indices)
+        new_words = self.get_words(self.guess, self.indices)  # get new words given indices
 
-        if MAX_PRINTED_WORDS:
-            if len(words) <= MAX_PRINTED_WORDS:
-                print("Current possible words: {}".format(", ".join(words)))
-        elif len(words) in (0, 1):
+        if MAX_PRINTED_WORDS:  # printing current/previous words
+            if len(new_words) not in (0, 1) and len(new_words) <= MAX_PRINTED_WORDS:
+                print("Your word might be {}".format(", ".join(new_words)))
+            elif len(new_words) > 1:
+                print("There are now {} possible words".format(len(new_words)))
+
+            if len(new_words) in (0, 1) and len(self.words) > MAX_PRINTED_WORDS:
+                print("Previous possible words: {}".format(", ".join(self.words)))
+
+        elif len(new_words) in (0, 1):
             print("Previous possible words: {}".format(", ".join(self.words)))
 
-        self.words = words
-        if self.check_words():
+        self.words = new_words
+        found = self.check_words()
+        if found:  # break from loop
             return True
-        self.decide_letter()
+        self.decide_letter()  # gets self.guess
 
     @staticmethod
-    def prune_database():
+    def prune_database():  # cleans a list of words
         allowed_letters = list("abcdefghijklmnopqrstuvwxyz")
 
-        words = open("english_words.txt", "r").read().split("\n")
+        words = open(WORDS_FILE, "r").read().split("\n")
 
         valid_words = list()
         for word in words:
-            if set(word) <= set(allowed_letters):
-                valid_words.append(word.lower())
+            if word and (set(word) <= set(allowed_letters)):  # ignore blanks and words with other characters
+                valid_words.append(word)
 
-        valid_words = list(set(valid_words))
-        valid_words.sort()
-        valid_words.sort(key = len)
+        valid_words = list(set(valid_words))  # remove duplicate words
+        valid_words.sort()  # sort by alphabet
+        valid_words.sort(key=len)  # sort by length
 
         current_len = 1
         wordset = list()
         for word in valid_words:
             if current_len != len(word):
-                wordset.append(" ")
+                wordset.append(" ")  # leave a linebreak if word length changes
                 current_len = len(word)
             wordset.append(word)
 
-        open("english_words.txt", "w").write("\n".join(wordset))
+        open(WORDS_FILE, "w").write("\n".join(wordset))
 
 
 #Guesser.prune_database()
 
-MAX_PRINTED_WORDS = 5
+LETTER_CHARACTERS = ("-", "=")
+MAX_PRINTED_WORDS = 8
+WORDS_FILE = "english_words.txt"
 
 while True:
     length = Guesser.ask_length()
